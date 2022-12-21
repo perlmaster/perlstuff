8f5bef5c410073900402174fe31fbc5727ca5981
Barry Kimelman <36140641+perlmaster@users.noreply.github.com>
2022-12-21 10:43:18 -0600

    Update list-table.pl

--- list-table.pl
+++ list-table.pl
@@ -22,40 +22,15 @@ use File::Spec;
 use FindBin;
 use lib $FindBin::Bin;
 use DBI;
+use database;
 
-require "mysql_utils.pl";
 require "print_lists.pl";
+require "mysql_utils.pl";
+require "display_pod_help.pl";
 
-my %options = ( "d" => 0 , "h" => 0 , "t" => 0 , "r" => -1 , "D" => "qwlc" );
+my %options = ( "d" => 0 , "h" => 0 , "t" => 0 , "r" => -1 , "D" => $database::db_name , "x" => 0 );
 my $dbh;
 my @column_headers = ();
-
-######################################################################
-#
-# Function  : debug_print
-#
-# Purpose   : Optionally print a debugging message.
-#
-# Inputs    : @_ - array of strings comprising message
-#
-# Output    : (none)
-#
-# Returns   : nothing
-#
-# Example   : debug_print("Process the files : ",join(" ",@xx),"\n");
-#
-# Notes     : (none)
-#
-######################################################################
-
-sub debug_print
-{
-	if ( $options{"d"} ) {
-		print join("",@_);
-	} # IF
-
-	return;
-} # end of debug_print
 
 ######################################################################
 #
@@ -79,10 +54,23 @@ sub dump_table
 {
 	my ( $table ) = @_;
 	my ( $sql , $sth , @colnames , $colname , $status , $num_cols , $i );
-	my ( $length , $column , $row , @headers );
-	my ( $ref_names , @rows , @row , $row_num );
+	my ( $length , $column , $row , @headers , $maxlen , $errmsg , $comment );
+	my ( $ref_names , @rows , @row , $row_num , $num_rows );
 
-	print "\n==  $table  ==\n\n";
+	$comment = get_table_comment($table,$options{"D"},\$errmsg,$dbh);
+	if ( defined $comment ) {
+		if ( $comment =~ m/\S/ ) {
+			$comment = " : $comment";
+		} # IF
+		else {
+			$comment = "";
+		} # ELSE
+	} # IF
+	else {
+		print "\n$errmsg\n";
+	} # ELSE
+
+	print "\n==  $table${comment}  ==\n\n";
 
 	$sql = "SELECT * FROM $table";
 
@@ -104,11 +92,13 @@ sub dump_table
 
 	@rows = ();
 	$row_num = 0;
+	$num_rows = 0;
 	while ( $row = $sth->fetchrow_hashref ) {
 		$row_num += 1;
 		if ( $options{'r'} > 0 && $row_num > $options{'r'} ) {
 			last;
 		} # IF
+		$num_rows += 1;
 		@row = ();
 		for ( $i = 0 ; $i <= $#colnames ; ++$i ) {
 			$colname = $colnames[$i];
@@ -128,7 +118,27 @@ sub dump_table
 	else {
 		@headers = @colnames;
 	} # ELSE
-	print_list_of_rows(\@rows,\@headers,"=",0,\*STDOUT);
+	$maxlen = (sort { $b <=> $a} map { length $_ } @headers)[0];
+
+	if ( $options{'x'} ) {
+		$row_num = 0;
+		foreach my $row ( @rows ) {
+			$row_num += 1;
+			@row = @$row;
+			print "\nRecord ${row_num}\n";
+			if ( $options{'n'} ) {
+				printf "Record %d\n",$row_num;
+			} # IF
+			for ( $i = 0 ; $i < $num_cols ; ++$i ) {
+				printf "%-${maxlen}.${maxlen}s %s\n",$headers[$i],$row[$i];
+			} # FOR
+		} # FOREACH
+	} # IF
+	else {
+		print "\n";
+		print_list_of_rows(\@rows,\@headers,"=",0,\*STDOUT);
+	} # ELSE
+	print "\n${num_rows} rows retrieved from table ${table}\n";
 
 	return 0;
 } # end of dump_table
@@ -153,28 +163,23 @@ sub dump_table
 
 MAIN:
 {
-	my ( $status );
+	my ( $status , $errmsg );
 
-	$status = getopts("dhc:C:tr:D:",\%options);
+	$status = getopts("dhc:C:tr:D:x",\%options);
 	if ( $options{"h"} ) {
-		if ( $^O =~ m/MSWin/ ) {
-# Windows stuff goes here
-			system("pod2text $0 | more");
-		} # IF
-		else {
-# Non-Windows stuff (i.e. UNIX) goes here
-			system("pod2man $0 | nroff -man | less -M");
-		} # ELSE
+		display_pod_help($0);
 		exit 0;
 	} # IF
 	unless ( $status && 0 < @ARGV ) {
-		die("Usage : $0 [-dht] [-D database] [-r rows_limit] [-C column_header_strings] [-c column_headers_file] table [... table]\n");
+		die("Usage : $0 [-dhtx] [-D database] [-r rows_limit] [-C column_header_strings] [-c column_headers_file] table [... table]\n");
 	} # IF
 	print "\n";
-	if ( $options{"t"} ) {
+
+	unless ( $options{"t"} ) {
 		$status = localtime;
 		print "$status\n\n";
-	} # IF
+	} # UNLESS
+
 	if ( exists $options{'c'} && exists $options{"C"} ) {
 		die("options 'c' and 'C' are mutually exclusive\n");
 	} # IF
@@ -191,7 +196,10 @@ MAIN:
 	} # IF
 
 	# connect to the database.
-	$dbh = mysql_connect_to_db($options{"D"},"127.0.0.1","root","archer-nx01");
+	$dbh = database::connect_to_mysql_database($options{"D"},"127.0.0.1",$database::db_user,$database::db_pass, \$errmsg);
+	unless ( defined $dbh ) {
+		die("Can't connect to database '$options{'D'}' as $database::db_user\n$errmsg\n");
+	} # UNLESS
 
 	foreach my $table ( @ARGV ) {
 		dump_table($table);
@@ -209,7 +217,7 @@ list-table.pl - Display the records in a table.
 
 =head1 SYNOPSIS
 
-list-table.pl [-hdt] [-D database] [-r rows_limit] [-C column_header_strings] [-c column_headers_file] table [... table]
+list-table.pl [-hdtx] [-D database] [-r rows_limit] [-C column_header_strings] [-c column_headers_file] table [... table]
 
 =head1 DESCRIPTION
 
@@ -225,9 +233,10 @@ Display the records in a table.
   -d - activate debugging mode
   -c filename - name of file containing list of column headers
   -C csv_list - comma separated list of column headers
-  -t - display a time/date message
+  -t - do not display a time/date message
   -r rows_limit - only display this many rows
   -D database - override default database
+  -x - list records in column format
 
 =head1 EXAMPLES
 
