4b765e3c9d5623fccd9141d5d7ada7c4212654cf
Barry Kimelman <36140641+perlmaster@users.noreply.github.com>
2023-01-12 10:35:54 -0600

    Update list_zip_file_members.pl

--- list_zip_file_members.pl
+++ list_zip_file_members.pl
@@ -1,4 +1,4 @@
-#!C:\Perl64\bin\perl.exe -w
+#!/usr/bin/perl -w
 
 ######################################################################
 #
@@ -52,6 +52,7 @@ use lib $FindBin::Bin;
 require "print_lists.pl";
 require "comma_format.pl";
 require "format_megabytes.pl";
+require "list_columns_style.pl";
 
 ######################################################################
 #
@@ -68,7 +69,10 @@ require "format_megabytes.pl";
 #
 # Returns   : nothing
 #
-# Example   : list_zip_file_members($zipfile,$pattern,{ "m" => 0 },\*STDOUT);
+# Example   : list_zip_file_members($zipfile,$pattern,{ "m" => 0 , "c" => 0 },\*STDOUT);
+#     -c - list ZIP file members in a compact list with only names displayed
+#     -C - list ZIP file members in a compact list with only names and size displayed
+#     -m - display size in terms of GB/MB/KB
 #
 # Notes     : (none)
 #
@@ -77,12 +81,16 @@ require "format_megabytes.pl";
 sub list_zip_file_members
 {
 	my ( $zipfile , $pattern , $ref_options , $handle ) = @_;
-	my ( @members , @filenames , @dates , @sizes , $num_members , $num_matched );
-	my ( $status , $filename , $basename , $td , $zip , @index , $bytes );
+	my ( $bin_time , $total_bytes , @bindates , $comma );
+	my ( @members , @filenames , @dates , @sizes , @perms , $num_members , $num_matched );
+	my ( $num_listed , $filename , $basename , $td , $zip , @index , $bytes , $title );
+	my ( $attr );
 
 #                   'externalFileName' => 'foo.zip',
 #                   'fileName' => 'charset.conv',
 #                   'lastModFileDateTime' => 1149111825,
+	print "\nlist_zip_file_members($zipfile)\n";
+
 	$zip = Archive::Zip->new();
 	if ( $zip->read( $zipfile ) != AZ_OK ) {
 		warn("Error reading zip file '$zipfile'\n");
@@ -91,17 +99,23 @@ sub list_zip_file_members
 	@members = $zip->members();
 	@filenames = ();
 	@dates = ();
+	@bindates = ();
 	@sizes = ();
+	@perms = ();
+
 	$num_members = scalar @members;
 	$num_matched = 0;
-	$status = 0;
+	$num_listed = 0;
+	$total_bytes = 0;
 	@index = ();
 	foreach my $element( @members ) {
-		$status += 1;
 		$filename = $element->{'fileName'};
+		$attr = $element->unixFileAttributes(); # get UNIX file attributes
 		$basename = basename($filename);
 		if ( $basename =~ m/${pattern}/i ) {
+			$num_listed += 1;
 			$bytes = $element->{'uncompressedSize'};
+			$total_bytes += $bytes;
 			$num_matched += 1;
 			push @index,$num_matched;
 			push @filenames,$filename;
@@ -111,14 +125,28 @@ sub list_zip_file_members
 			else {
 				push @sizes,comma_format($bytes);
 			} # ELSe
-			$td = localtime($element->lastModTime());
+			$bin_time = $element->lastModTime();
+			$td = localtime($bin_time);
 			push @dates,$td;
+			push @bindates,$bin_time;
+			push @perms,sprintf "0%o",$attr; # save UNIX file attributes as octal
 		} # IF
 	} # FOREACH
+	$bytes = format_megabytes($total_bytes,1);
 
-	@index = map { "[$_ / $num_matched]" } (1 .. $num_matched);
-	print $handle "\n$zipfile : $num_matched of the $num_members members were matched by '$pattern'\n\n";
-	print_lists([ \@index , \@filenames , \@sizes , \@dates],[ "#" , "Member","Size","Date" ],"=",$handle);
+	@index = map { "$_/$num_matched" } (1 .. $num_matched);
+	$title = "$zipfile : $num_matched of the $num_members members were matched by '$pattern'\n";
+	if ( $ref_options->{'c'} ) {
+		print_lists( [ \@index , \@filenames ] , [ '#' , 'Member' ] , '=' , $handle);
+	} elsif ( $ref_options->{"C"} ) {
+		print_lists( [ \@index , \@filenames , \@sizes ] , [ '#' , 'Member' , 'Size' ] , '=' , $handle);
+	} else {
+		print $handle "\n$title\n";
+		print_lists([ \@index , \@filenames , \@sizes , \@dates , \@bindates , \@perms ],
+					[ "#" , "Member","Size","Date" , "Binary Date" , "Permissions" ],"=",$handle);
+	} # ELSE
+	$comma = comma_format($total_bytes);
+	print "\n${comma} bytes [ $bytes ] in ${num_matched} listed members\n";
 
 	return;
 } # end of list_zip_file_members
